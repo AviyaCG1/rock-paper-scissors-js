141606e0148d36119ed9f6fcc11e94b5800fa27b
AviyaCG <aviyacg@gmail.com>
2022-09-08 19:50:49 +0300

    Add getComputerChoice

--- rps-game.js
+++ rps-game.js
@@ -1,1 +1,30 @@
 
+// return a number between 0 to (num-1)
+function random(num){
+    return Math.floor(Math.random() * num);
+}
+
+function getComputerChoice(){
+    // Generate a random int 0,1,2
+    randIndex = random(3);
+    // IF int is 0 RETURN paper
+    // IF int is 1 RETURN scissors
+    // IF int is 2 RETURN rock
+    choiceIndex = ["paper", "scissors", "rock"];
+    return choiceIndex[randIndex];
+}
+
+function whoWon(playerChoice, computerChoice){
+    // Check if its tie
+    // IF player won RETURN "You Won! {playerChoice} beats {computerChoice}"
+    // ELSE "You lose! {computerChoice} beats {playerChoice}"
+
+}
+
+function playRound(playerChoice, computerChoice){
+    // LowerCase playerChoice
+    // Check who won
+    // RETURN text
+}
+
+
